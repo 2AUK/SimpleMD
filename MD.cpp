07d47a3826c1068aada9558dbb72ddcb14496c68
abdullah <gxb14174@uni.strath.ac.uk>
2018-10-23 23:14:46 +0100

    Figured out how to generate a 3D cube of points lmao

--- MD.cpp
+++ MD.cpp
@@ -1,32 +1,61 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
 
-std::vector<float> lattice_pos(int, float);
+/*Simple MD code for simple monoatomic liquids
+ *Energy and Force calculated using Lennard-Jones
+ */
+
+struct particle{
+    std::vector<float> coordinates;
+    std::vector<float> velocities;
+    std::vector<float> forces;
+};
+
+std::vector<particle> lattice_pos(int, float);
+void print_particle_info(std::vector<particle>);
 
 int main(){
-    std::vector<float> newc = lattice_pos(5, 0.5);
-    for (auto it = newc.begin(); it != newc.end(); it++){
-        float x = *std::next(it, 0); float y = *std::next(it, 1); float z = *std::next(it, 2);
-        std::cout << x << '\t' << y << '\t' << z << '\n';
-        std::advance(it, 2);
-    }
+    std::vector<particle> particles = lattice_pos(5, 1);
+    print_particle_info(particles);
     return 0;
 }
 
-//2D arrays [[0, 0, 0], [0, 0, 0.5], ..., [5, 5, 5]] for however many particles there are.
-//Essentially building a lattice of particles. This would be easiest by building a cube 
-//using whatever number evenly divides in to the number of particles
-//probably the stupidest thing i've ever done
-std::vector<float> lattice_pos(int npart, float grid_spacing){
-    std::vector<float> coordinates;
-    for (float x = 0; x <= npart*grid_spacing; x+= grid_spacing){
-        for (float y = 0; y <= npart*grid_spacing; y+= grid_spacing){
-            for (float z = 0; z <= npart*grid_spacing; z += grid_spacing){
-                coordinates.insert(coordinates.end(), {x, y, z});
+//What kind of arcane trickery is involved in generating a 3D cube of points
+//End me
+std::vector<particle> lattice_pos(int npart, float grid_spacing){
+    std::vector<particle> particles;
+    int counter = 0;
+    for (float x = 0; x < npart*grid_spacing; x+=grid_spacing){
+        for (float y = 0; y < npart*grid_spacing; y+=grid_spacing){
+            for (float z = 0; z < npart*grid_spacing; z +=grid_spacing){
+                particles.push_back({{x, y, z}, {0, 0, 0}, {0, 0, 0}});
             }
         }
     }
     std::cout << "Lattice Gen Routine Done\n";
-    return coordinates;
+    return particles;
+}
+
+void print_particle_info(std::vector<particle> particles){
+    int counter = 0;
+    for (auto const i : particles){
+        std::cout << "Particle " << counter << '\n'; 
+        std::cout << "Coordinates\n";
+        for (auto const coord : i.coordinates){
+            std::cout << coord << '\t';
+        }
+        std::cout << "\nVelocities\n";
+        for (auto const vels : i.velocities){
+            std::cout << vels << '\t';
+        }
+        std::cout << "\nForces\n";
+        for (auto const forces : i.forces){
+            std::cout << forces << '\t';
+        }
+        std::cout << std::endl;
+        counter++;
+    }
+}
+void initialise(int nparts, std::vector<float>* coordinates, std::vector<float>* velocities){
+
 }
